b2c09274be24bf9f4b3df3de939ef4d5616405bc
ritaly <malgorzata.lyczywek@gmail.com>
2023-05-11 18:14:58 +0200

    fun 07

--- 04_functions/all_07.py
+++ 04_functions/all_07.py
@@ -5,14 +5,9 @@
 # MasterCard numbers either start with the numbers 51 through 55 or with the numbers 2221 through 2720. All have 16 digits.
 #
 # American Express card numbers start with 34 or 37 and have 15 digits.
-#
 
 def is_card_number(num):
-    if num.isdigit():
-        return True
-    else:
-        return False
-
+    return num.isdigit()
 
 def get_card_number():
     while True:
@@ -28,10 +23,43 @@ def get_card_number():
     return card_nr
 
 
+def is_visa(card_num):
+    """Function that checks visa numbers"""
+    if card_num[0] == '4' and (len(card_num) == 16 or len(card_num) == 13):
+        return True
+    else:
+        return False
+
+
+def is_mastercard(card_num):
+    """Function that checks mastercard numbers"""
+    start_condition = int(card_num[0:2]) in range(51, 56) or int(card_num[0:4]) in range(2221, 2721)
+
+    if len(card_num) == 16 and start_condition:
+        return True
+    else:
+        return False
+
+
+def is_amex(card_num):
+    """Function that checks amex numbers"""
+    # American Express card numbers start with 34 or 37 and have 15 digits.
+    if len(card_num) == 15 and card_num[0:2] in ('34', '37'):
+        return True
+    else:
+        return False
+
+# --- main part of the program
+
 number = get_card_number()
 print('💳 :', number)
-# 2 func -> czy to jest karta visa
 
-# 3. func ->  czy to master card
+if is_visa(number):
+    print("This is Visa card number")
+elif is_mastercard(number):
+    print("This is MasterCard card number")
+elif is_amex(number):
+    print("This is AmericanExpress card number")
+else:
+    print("Unknown card number")
 
-# 4 func -> czy to american express
