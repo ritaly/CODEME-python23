aea50d1f83063464e5be8c2aa9fad7f100c0bf20
ritaly <malgorzata.lyczywek@gmail.com>
2023-05-11 19:30:59 +0200

    fun 01

--- 05_functions_cd/01.py
+++ 05_functions_cd/01.py
@@ -14,23 +14,27 @@ def show_month(name, days):
 
     print()
 
-data = [
-    ('January', range(31)),
-    ('February', range(28)),
-    ('March', range(31)),
-    ('April', range(30)),
-    ('May', range(31)),
-    ('June', range(30)),
-    ('July', range(31)),
-    ('August', range(31)),
-    ('September', range(30)),
-    ('October', range(31)),
-    ('November', range(30)),
-    ('December', range(31)),
-]
 
-# for row in data
-for (month_name, month_days) in data:
-    # a, b = row
-    show_month(month_name, month_days)
-    print()
+def main():
+    data = [
+        ('January', range(31)),
+        ('February', range(28)),
+        ('March', range(31)),
+        ('April', range(30)),
+        ('May', range(31)),
+        ('June', range(30)),
+        ('July', range(31)),
+        ('August', range(31)),
+        ('September', range(30)),
+        ('October', range(31)),
+        ('November', range(30)),
+        ('December', range(31)),
+    ]
+
+    # for row in data
+    for (month_name, month_days) in data:
+        # a, b = row
+        show_month(month_name, month_days)
+        print()
+
+main()
