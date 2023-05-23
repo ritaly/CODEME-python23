5907de04441650d9bbf13cbea152c8146b6aa536
ritaly <malgorzata.lyczywek@gmail.com>
2023-05-23 20:06:08 +0200

    zad 7

---
+++ 06_files/intro/07.py
@@ -0,0 +1,30 @@
+# Wisielec.
+# Utwórz plik zawierający listę słów wg. kategorii np. zwierzęta, owoce etc.
+
+# Poproś użytkownika o podanie kategorii przed rozpoczęciemy gry.
+"""
+Podaj kategorię:
+1 - zwierzęta
+2 - owoce
+3 - warzywa
+"""
+
+# <- 1
+
+# Następny wczytaj listę haseł do programu, wylosuj jedno hasło z listy.
+# - wyczutjemy linijkę "0" z pliku i dzielimy znakiem podziału
+# Stworzona lista elementów
+
+# użytkownik ma 6 żyć
+
+# Wylosować 1 słowo "to_guess_word"
+
+# Przedstawić jako - - - - - - - "hidden_word"
+
+# Zapytac użytkownika o literkę
+
+# - jak zgadnie literkęto podmienić - - - a - - - a
+# jak nie trafi to odjąć mu życie
+
+# Na koniec sprawdzić czy wykorzystał życia
+# - jęsli tak to "game over"
