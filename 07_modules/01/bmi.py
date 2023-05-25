f1635304e6a783291d5d712d318c6b70b672e24a
ritaly <malgorzata.lyczywek@gmail.com>
2023-05-25 19:33:02 +0200

    tasks 01

--- 07_modules/01/bmi.py
+++ 07_modules/01/bmi.py
@@ -1,10 +1,13 @@
-# Utwórz plik bmi.py zawierający metodę obliczania bmi.
-# Metoda zwraca wartości: niedowaga, waga normalna, nadwaga, otyłość.
-#
-# Utwórz 4 pliki .txt zawierające porady.
-#
-# Utwórz plik fitmeter.py, który zaimportuje moduł bmi.
-#
-# fitmeter.py pobierze wagę i wzrost pacjenta i przekaże do odpowiedniej funkcji z modułu bmi.
-#
-# Na podstawie zwróconej wartości fitmeter.py wyświetli odpowiednie porady dla pacjenta.
+def calc_bmi(height, weight):
+    return round(weight / height ** 2, 2)
+
+
+def get_bmi_status(bmi):
+    if bmi < 18.5:
+        return "underweight"
+    elif bmi < 25:
+        return "standard"
+    elif bmi < 30:
+        return "overweight"
+    else:
+        return "obesity"
