929f6a71bd6af8446eb9f505f43ba8d4e56afeeb
ritaly <malgorzata.lyczywek@gmail.com>
2023-05-25 19:30:11 +0200

    tasks 07

---
+++ 07_modules/01/bmi.py
@@ -0,0 +1,10 @@
+# Utwórz plik bmi.py zawierający metodę obliczania bmi.
+# Metoda zwraca wartości: niedowaga, waga normalna, nadwaga, otyłość.
+#
+# Utwórz 4 pliki .txt zawierające porady.
+#
+# Utwórz plik fitmeter.py, który zaimportuje moduł bmi.
+#
+# fitmeter.py pobierze wagę i wzrost pacjenta i przekaże do odpowiedniej funkcji z modułu bmi.
+#
+# Na podstawie zwróconej wartości fitmeter.py wyświetli odpowiednie porady dla pacjenta.
