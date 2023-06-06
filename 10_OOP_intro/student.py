7ce583492fc716b13737d997b7aaa52b7b27df41
ritaly <malgorzata.lyczywek@gmail.com>
2023-06-06 18:14:02 +0200

    OOP intro

---
+++ 10_OOP_intro/student.py
@@ -0,0 +1,29 @@
+class Student:
+    school = 'UAM'
+    def __init__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+    def email(self):
+        return f'{self.lastname}.{self.firstname[0]}@{self.school.lower()}.pl'
+
+    def sound(self, number):
+        return f'go {self.school}!' * number
+
+
+
+anna = Student('anna', 'nowak', 23)
+adam = Student('adam', 'snow', 22)
+
+print(anna.email())
+print(adam.email())
+
+anna.lastname = 'kowal'
+print(anna.lastname)
+print(anna.email())
+
+print(anna.sound(2))
+
+print(type(6))
+print(type(anna))
