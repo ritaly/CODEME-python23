b3efda7063028d2adcd8c78b55f6ac947642b746
ritaly <malgorzata.lyczywek@gmail.com>
2023-06-06 18:23:07 +0200

    OOP intro

--- 10_OOP_intro/student.py
+++ 10_OOP_intro/student.py
@@ -5,25 +5,30 @@ class Student:
         self.lastname = lastname
         self.age = age
 
+    def __str__(self):
+        email = self.email()
+        return f'imię: {self.firstname}; nazwisko: {self.lastname}, wiek: {self.age}, email: {email}'
+
     def email(self):
         return f'{self.lastname}.{self.firstname[0]}@{self.school.lower()}.pl'
 
     def sound(self, number):
         return f'go {self.school}!' * number
 
-
-
 anna = Student('anna', 'nowak', 23)
 adam = Student('adam', 'snow', 22)
 
-print(anna.email())
-print(adam.email())
+print(adam)
+#
+# print(anna.email())
+# print(adam.email())
+#
+# anna.lastname = 'kowal'
+# print(anna.lastname)
+# print(anna.email())
+#
+# print(anna.sound(2))
+#
+# print(type(6))
+# print(type(anna))
 
-anna.lastname = 'kowal'
-print(anna.lastname)
-print(anna.email())
-
-print(anna.sound(2))
-
-print(type(6))
-print(type(anna))
